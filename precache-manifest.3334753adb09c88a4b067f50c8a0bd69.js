faa4810b653bd16cbf352cb44b04e9785ab9f22b
root <pratikdeshmukhlobhi@gmail.com>
2020-05-17 14:44:18 +0530

    Updates

---
+++ precache-manifest.3334753adb09c88a4b067f50c8a0bd69.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9365d8dc06f37e62885c10c52c6758ed",
+    "url": "/snake-game/index.html"
+  },
+  {
+    "revision": "f4ff3f5578cf6e9a2a5a",
+    "url": "/snake-game/static/css/main.45a12b5a.chunk.css"
+  },
+  {
+    "revision": "d0bc3709b3e87cfa4b63",
+    "url": "/snake-game/static/js/2.6a2de437.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snake-game/static/js/2.6a2de437.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4ff3f5578cf6e9a2a5a",
+    "url": "/snake-game/static/js/main.c5fdaa75.chunk.js"
+  },
+  {
+    "revision": "dcbca614602b177c2e27",
+    "url": "/snake-game/static/js/runtime-main.833ca6c1.js"
+  },
+  {
+    "revision": "b685cef5416e5914b679a70c40a0f0b7",
+    "url": "/snake-game/static/media/Tick-DeepFrozenApps-397275646.b685cef5.mp3"
+  },
+  {
+    "revision": "49d382a577d83c95cf055ede36f26e9a",
+    "url": "/snake-game/static/media/game-over-sound-effect.49d382a5.mp3"
+  },
+  {
+    "revision": "cd6b4dbb8b0858ef4803a85276430734",
+    "url": "/snake-game/static/media/zapsplat_human_eat_bite_apple_004_15223.cd6b4dbb.mp3"
+  }
+]);
